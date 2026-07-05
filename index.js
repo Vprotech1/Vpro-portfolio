b7bbf3c6b994d8a45da178d0d0791da64ec0f073
Vprotech1 <viesscott@gmail.com>
2026-07-05 17:35:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 let load = document.getElementById('load');
 
-window.addEventListener('load', function(){
-    setTimeout(function(){
-        load.style.display ='none';
-    },1500);
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        load.style.display = 'none';
+    }, 1500);
 })
 //
 
-let brocode =document.getElementById('vpr');
+let brocode = document.getElementById('vpr');
 let navbar = document.getElementById('navbar');
-let  navbarbrand = document.getElementById('navbarbrandimg');
+let navbarbrand = document.getElementById('navbarbrandimg');
 
-window.addEventListener('scroll', function(){
-    if(window.scrollY > 150){
-        navbar.style.background= "linear-gradient( rgb(0,0,0,0.3))";
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        navbar.style.background = "linear-gradient( rgb(0,0,0,0.3))";
         navbar.style.backdropFilter = "blur(20px)"
-        navbarbrand.src="photos/logo2.png";
     }
-    else{
-        navbar.style.backgroundColor="#000";
-        navbarbrand.src="photos/logo1.png";
+    else {
+        navbar.style.backgroundColor = "#000";
     }
 })
 // 
@@ -28,18 +26,19 @@ let testimain = document.getElementById('testimain');
 let btnsreview1 = document.getElementById('btnsreview1');
 let btnsreview2 = document.getElementById('btnsreview2');
 
-btnsreview1.addEventListener('click', function(){
-    testimain.scrollLeft +=380;
-   
+btnsreview1.addEventListener('click', function () {
+    testimain.scrollLeft += 380;
+
 })
-btnsreview2.addEventListener('click', function(){
-    testimain.scrollLeft -=380;
-   
+btnsreview2.addEventListener('click', function () {
+    testimain.scrollLeft -= 380;
+
 })
 //
 //
 
 let lightdark = document.getElementById('lightdark');
-lightdark.addEventListener('click', function(){
+lightdark.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
 })
+
